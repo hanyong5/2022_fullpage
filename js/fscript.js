32cd8e62fd12fd3f0d1b355f8ea8e04ebe600059
hanyong5 <hanyong5@naver.com>
2022-08-30 00:02:34 +0900

    20220830

--- js/fscript.js
+++ js/fscript.js
@@ -1,8 +1,10 @@
 $(".logo_b").hide()
+
 $("#fullpage").fullpage({
     menu: ".topMenu",
     anchors: ["m1st", "m2st", "m3st", "m4st", "m5st", "m6st"],
     navigation:false,
+    css3:true,
     //scrollOverflow:true,
     responsiveWidth:1000,
     navigationTooltips:['home','company','product','cummunity'],
@@ -13,7 +15,7 @@ $("#fullpage").fullpage({
             $(".logo_b").show()
             $(".t1").addClass("move");
             $(".navbar .gnb ul").removeClass("view")
-            $(".scene").css("display","none")
+            $(".scene").css("opacity",0)
         }else{
             $(".t1").removeClass("move")
         }
@@ -21,25 +23,36 @@ $("#fullpage").fullpage({
         if(index==2){
             $(".navbar .gnb ul").addClass("view")
             $(".logo_b").hide()
-            $(".scene").css("display","none")
+            $(".scene").css("opacity",0)
         }
-        if(index==3){
+        if(index>=3){
+            $(".navbar").addClass("white")
             $(".navbar .gnb ul").addClass("view");
-            $(".scene").css("display","flex")
+            $(".scene").css("opacity",1)
             $(".back03 .txt1,.back03 .txt2,.back03 .txt3").addClass("view");
         }else{
-            //$(".back03 .txt1,.back03 .txt2,.back03 .txt3").removeClass("view");
-            // $(".scene").css("display","none")
+            $(".back03 .txt1,.back03 .txt2,.back03 .txt3").removeClass("view");
+            // $(".scene").css("opacity",0)
         }
 
         if(index==4){
-            $(".scene.back03").addClass("tAdd01")
+            $(".scene.back03").addClass("fade");
+            $(".navbar").removeClass("white");
+            $(".scene.back04 .text-view01").addClass("sizeDown")
+            $(".scene.back04 .viewTxtWrap").addClass("txtMove")
+            $(".scene.back04 .product1").addClass("p1view")
         }else{
-            $(".scene.back03").removeClass("tAdd01").removeClass("tAdd02")
+            $(".scene.back03").removeClass("fade");
+
+            $(".scene.back04 .text-view01").removeClass("sizeDown")
+            $(".scene.back04 .viewTxtWrap").removeClass("txtMove")
+            $(".scene.back04 .product1").removeClass("p1view")
+            // $(".scene.back03").removeClass("tAdd01").removeClass("tAdd02")
         }
 
         if(index==5){
-            $(".scene.back03").addClass("tAdd01").addClass("tAdd02");
+            $(".scene.back03").css("opacity",0);
+
         }else{
             //$(".scene.back03").removeClass("tAdd01").removeClass("tAdd02")
         }
